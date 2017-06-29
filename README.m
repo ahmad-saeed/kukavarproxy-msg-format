1eb10bd7aba392b5c9a157e861ea7648dd7d9772
Ahmad Saeed <ahmad._.saeed@outlook.com>
2017-06-29 15:47:37 +0200

    readme initial commit

---
+++ README.m
@@ -0,0 +1,12 @@
+# kukavarproxy Message Format
+
+## 1. Description
+
+
+## 2. How it works
+
+
+## 3. Resources
+
+* http://sourceforge.net/projects/openshowvar/
+* https://github.com/aauc-mechlab/jopenshowvar/
